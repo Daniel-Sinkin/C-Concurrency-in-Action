fb59636d9444ce8f29ce5d3351be87e5cf5d947d
Daniel-Sinkin <danielsinkin97@gmail.com>
2025-05-27 03:10:43 +0200

    Writing simple thread sleep program

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <print>
 #include <thread>
+#include <vector>
 
-auto hello() -> void {
-    std::println("Hello, World!");
+#include <chrono>
+using namespace std::chrono_literals;
+
+constexpr size_t max_num_threads = 3;
+
+auto hello(size_t thread_idx) -> void {
+    std::println("Thread {} got created.", thread_idx);
+    std::this_thread::sleep_for(1.0s * (thread_idx + 1));
+    std::println("Thread {} Finished Sleeping", thread_idx);
 }
 
-auto main() -> int {
-    std::thread t(hello);
-    t.join();
+int main() {
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < max_num_threads; ++i) {
+        threads.emplace_back([i]() { hello(i); });
+    }
+    for (auto &thread : threads) {
+        thread.join();
+    }
 }
